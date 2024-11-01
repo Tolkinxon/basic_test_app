b1321d47040b0770b637f2f7412c71422bfedb4e
Soliyev Tolkinxon <krosstony639@gmail.com>
2024-11-01 10:13:59 +0500

    add fs module

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
-const user = require('./data')
+const user = require('./data');
+const path = require('path');
+const fs = require('fs');
 
-console.log(user);
+const fileName = path.basename(__filename);
+const dirName = path.dirname(__filename);
+const extName = path.extname(__dirname)
+const parse = path.parse(__filename)
+const join = path.join(__dirname, "\hello.jj", "hello.kl")
+const resolve = path.resolve(__dirname, "/hello", "hello.kl")
+
+// fs.mkdir(path.join(__dirname, "hel"), (err) => {
+//     if(err) throw err;
+//     console.log('folder created');    
+// })  
+
+fs.writeFile(path.join(__dirname, "test", 'index.js'), "//hello tolkinxon", (err) => {
+    if(err)  throw err;    
+    console.log('file created successfully');    
+})
