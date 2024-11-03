b288befc1fecc689614b9502472e942ea6b9b07b
Soliyev Tolkinxon <krosstony639@gmail.com>
2024-11-03 17:38:29 +0500

    Add cors configuration

--- js/index.js
+++ js/index.js
@@ -3,6 +3,13 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res)=>{
+
+    const headers = {
+        'Access-Control-Allow-Origin': '*', /* @dev First, read about security */
+        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+      };
+
+
     if(req.url === '/'){
         res.end(`<h1>hello world</h1>`);
     }
@@ -12,18 +19,19 @@ const server = http.createServer((req, res)=>{
     }
 
     if(req.url === '/api/users'){
+        res.writeHead(200, headers);
         fs.readFile(
             path.join(path.dirname(__dirname), "data_base", "users.json"),  
             
             (err, data) => {
             if(err) throw err
-            console.log(Buffer.from(data));  
-            res.end();          
+            res.end(Buffer.from(data).toString());          
             })
-    }
-
+        }
 });
 
 server.listen('5000',()=>{
     console.log('server initialized');    
 });
+
+
