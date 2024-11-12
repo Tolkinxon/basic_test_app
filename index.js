ad447ec55d33f89a498c4e4def87183184bdc9ef
Soliyev Tolkinxon <krosstony639@gmail.com>
2024-11-12 12:31:57 +0500

    Add questions

--- index.js
+++ index.js
@@ -23,14 +23,15 @@ const server = http.createServer( async (req, res)=>{
         req.on('end', async ()=>{
             let users = await fs.readFile(path.join("database", "users.json"), "UTF-8");
                 users = users ? JSON.parse(users) : [];
-                
+
+            user = {...user, page:1, isCorrect: 0}
             const compareFn = item => item.email == user.email
-            if(users.some(compareFn)) res.end(JSON.stringify({message: "This user already excist", state: false}));
+            if(users.some(compareFn)) res.end(JSON.stringify({message: "This email already excist", state: false}));
             else {
                 users.push(user);
                 fs.writeFile(path.join("database", "users.json"), JSON.stringify(users)); 
                         
-                res.end(JSON.stringify({message: "This user successfully added", state: true}));
+                res.end(JSON.stringify({message: "This user successfully added", state: true, user:{...user}}));
             }
         })
     } 
@@ -47,9 +48,9 @@ const server = http.createServer( async (req, res)=>{
             let users = await fs.readFile(path.join("database", "users.json"), "UTF-8");
                 users = users ? JSON.parse(users) : [];
                 
-            const compareFn = item => item.email == user.email
-            if(users.some(compareFn)){
-                res.end(JSON.stringify({message: 'Checking was successfully', state: true}));
+            const foundedUser = users.find(item => item.email == user.email)
+            if(foundedUser){
+                res.end(JSON.stringify({message: 'Checking was successfully', state: true, user:{...foundedUser}}));
             }
             else {
                 res.end(JSON.stringify({message: "We don't found your email please sign up", state: false}));
@@ -66,7 +67,7 @@ const server = http.createServer( async (req, res)=>{
     else if(req.url.includes('/questions')){
         res.writeHead(200, headers);
         let questions = await fs.readFile(path.join("database", "questions.json"),  "UTF-8")
-              questions = questions ? JSON.parse(questions) : [];
+            questions = questions ? JSON.parse(questions) : [];
         const page = req.url.at(-1)
         const currentQuestionPage = questions[page - 1];  
         res.end(JSON.stringify(currentQuestionPage))
